3052394d9198fc7fd0c146d2950177bbd92e9076
Leila <leila.alhemali@gmail.com>
2026-01-06 13:42:09 -0700

    change port

--- index.js
+++ index.js
@@ -94,11 +94,12 @@ app.get("/info/:deviceIdentifier", async (req, res) => {
   let cam;
   try {
     cam = await getCameraByIdentifier(req.params.deviceIdentifier);
+    console.log(cam, "cam in info/:deviceIdentifier");
     const ranges = await getAllRanges(cam);
     res.send(ranges);
   } catch (err) {
     console.error(err);
-    res.status(500).send(err);
+    if (err.messag) res.status(500).send(err);
     return;
   }
 });
@@ -146,6 +147,7 @@ app.get("/set/:deviceIdentifier/:control/:values", async (req, res) => {
   let values = req.params.values.split(",");
   const cam = await getCameraByIdentifier(req.params.deviceIdentifier);
   const controlObj = await cam.get(req.params.control);
+  console.log(controlObj);
   const currentValue = Object.values(controlObj)[0];
   console.log("setting", req.params.control, values);
 
@@ -172,6 +174,6 @@ app.get("/set/:deviceIdentifier/:control/:values", async (req, res) => {
   }
 });
 
-app.listen(3456, () => {
-  console.log("Server listening on port 3456");
+app.listen(8080, () => {
+  console.log("Server listening on port 8080");
 });
